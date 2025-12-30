9d7224d2c6e3fe46257f5eef56c811b12987fba7
nyfreshbuy <dailinqi888@gmail.com>
2025-12-30 00:10:48 -0500

    fix: normalize user model import in admin_auth

--- backend/src/routes/admin_auth.js
+++ backend/src/routes/admin_auth.js
@@ -2,7 +2,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 router.use(express.json());
